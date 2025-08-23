f4c51693ed45b3242878585a5af80e19d8cf461d
adam-sion <adamsion74@gmail.com>
2025-08-23 22:34:15 +0300

    fix: sameSite none cookie

--- back/ecom-enterprise/src/main/java/adam/dev/ecom_enterprise/util/CookieUtil.java
+++ back/ecom-enterprise/src/main/java/adam/dev/ecom_enterprise/util/CookieUtil.java
@@ -39,7 +39,7 @@ public class CookieUtil {
                 .httpOnly(true)
                 .path("/")
                 .secure(true)
-                .sameSite("Lax")
+                .sameSite("None")
                 .build();
 
         response.addHeader("Set-Cookie", cookie.toString());
